b047c60e3c43fd1b76e6ecd8e3f3a0d634ca0b58
jpmoraess <moraess.it@gmail.com>
2022-11-23 09:39:20 -0300

    Implementing Order Service domain logic using DDD - Order Aggregate Root

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/valueobject/StreetAddress.java
@@ -0,0 +1,53 @@
+package com.food.ordering.system.order.service.domain.valueobject;
+
+import java.util.UUID;
+
+public class StreetAddress {
+    private final UUID id;
+    private final String street;
+    private final String postalCode;
+    private final String city;
+
+    public StreetAddress(UUID id, String street, String postalCode, String city) {
+        this.id = id;
+        this.street = street;
+        this.postalCode = postalCode;
+        this.city = city;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        StreetAddress that = (StreetAddress) o;
+
+        if (!street.equals(that.street)) return false;
+        if (!postalCode.equals(that.postalCode)) return false;
+        return city.equals(that.city);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = street.hashCode();
+        result = 31 * result + postalCode.hashCode();
+        result = 31 * result + city.hashCode();
+        return result;
+    }
+}
