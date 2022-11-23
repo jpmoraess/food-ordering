31abfe532d81f739e842c8b78417c9e5e78ad9a0
jpmoraess <moraess.it@gmail.com>
2022-11-23 12:02:38 -0300

    Implementing Order Domain Service

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Product.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Product.java
@@ -8,6 +8,11 @@ public class Product extends BaseEntity<ProductId> {
     private String name;
     private Money price;
 
+    public void updateWithConfirmedNameAndPrice(String name, Money price) {
+        this.name = name;
+        this.price = price;
+    }
+
     public Product(ProductId productId, String name, Money price) {
         super.setId(productId);
         this.name = name;
