374ad35566528c0c83050d69fa6e43b8ac8e342b
jpmoraess <moraess.it@gmail.com>
2022-11-24 19:32:26 -0300

    Implementing input ports in Order Application Service

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/repository/CustomerRepository.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/repository/CustomerRepository.java
@@ -1,11 +1,11 @@
 package com.food.ordering.system.order.service.domain.ports.output.repository;
 
-import com.food.ordering.system.domain.valueobject.CustomerId;
 import com.food.ordering.system.order.service.domain.entity.Customer;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface CustomerRepository {
 
-    Optional<Customer> findCustomer(CustomerId customerId);
+    Optional<Customer> findCustomer(UUID customerId);
 }
