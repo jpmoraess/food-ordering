84ad777b3a444352859430f9e07732efd97cde5a
jpmoraess <moraess.it@gmail.com>
2022-12-10 00:49:32 -0300

    Implementing container module for Payment Service

---
+++ payment-service/payment-container/src/main/java/com/food/ordering/system/payment/service/domain/PaymentServiceApplication.java
@@ -0,0 +1,16 @@
+package com.food.ordering.system.payment.service.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages = "com.food.ordering.system")
+@EntityScan(basePackages = "com.food.ordering.system.payment.service.dataaccess")
+@EnableJpaRepositories(basePackages = "com.food.ordering.system.payment.service.dataaccess")
+public class PaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceApplication.class, args);
+    }
+}
