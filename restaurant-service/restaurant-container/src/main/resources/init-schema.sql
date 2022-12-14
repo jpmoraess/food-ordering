4da1d54c751939b13b7a5dbb9fd40a67386efabf
jpmoraess <moraess.it@gmail.com>
2022-12-14 13:03:53 -0300

    Fix some database script errors and modules in pom.xml

--- restaurant-service/restaurant-container/src/main/resources/init-schema.sql
+++ restaurant-service/restaurant-container/src/main/resources/init-schema.sql
@@ -1,9 +1,11 @@
 DROP SCHEMA IF EXISTS restaurant CASCADE;
+
 CREATE SCHEMA restaurant;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 DROP TABLE IF EXISTS restaurant.restaurants CASCADE;
+
 CREATE TABLE restaurant.restaurants
 (
     id uuid NOT NULL,
@@ -13,11 +15,11 @@ CREATE TABLE restaurant.restaurants
 );
 
 DROP TYPE IF EXISTS approval_status;
+
 CREATE TYPE approval_status AS ENUM ('APPROVED', 'REJECTED');
 
--- order approval table
-
 DROP TABLE IF EXISTS restaurant.order_approval CASCADE;
+
 CREATE TABLE restaurant.order_approval
 (
     id uuid NOT NULL,
@@ -27,9 +29,8 @@ CREATE TABLE restaurant.order_approval
     CONSTRAINT order_approval_pkey PRIMARY KEY (id)
 );
 
--- products table
-
 DROP TABLE IF EXISTS restaurant.products CASCADE;
+
 CREATE TABLE restaurant.products
 (
     id uuid NOT NULL,
@@ -39,9 +40,8 @@ CREATE TABLE restaurant.products
     CONSTRAINT products_pkey PRIMARY KEY (id)
 );
 
--- restaurant products table
-
 DROP TABLE IF EXISTS restaurant.restaurant_products CASCADE;
+
 CREATE TABLE restaurant.restaurant_products
 (
     id uuid NOT NULL,
@@ -50,8 +50,6 @@ CREATE TABLE restaurant.restaurant_products
     CONSTRAINT restaurant_products_pkey PRIMARY KEY (id)
 );
 
--- foreign keys
-
 ALTER TABLE restaurant.restaurant_products
     ADD CONSTRAINT "FK_RESTAURANT_ID" FOREIGN KEY (restaurant_id)
     REFERENCES restaurant.restaurants (id) MATCH SIMPLE
@@ -59,31 +57,29 @@ ALTER TABLE restaurant.restaurant_products
     ON DELETE RESTRICT
     NOT VALID;
 
-ALTER TABLE restaurant.restaurants_products
+ALTER TABLE restaurant.restaurant_products
     ADD CONSTRAINT "FK_PRODUCT_ID" FOREIGN KEY (product_id)
     REFERENCES restaurant.products (id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE RESTRICT
     NOT VALID;
 
--- materialized view
-
 DROP MATERIALIZED VIEW IF EXISTS restaurant.order_restaurant_m_view;
 
 CREATE MATERIALIZED VIEW restaurant.order_restaurant_m_view
 TABLESPACE pg_default
 AS
-    SELECT r.id AS restaurant_id,
-        r.name AS restaurant_name,
-        r.active AS restaurant_active,
-        p.id AS product_id,
-        p.name AS product_name,
-        p.price AS product_price,
-        p.available AS product_available
-    FROM restaurant.restaurants r,
-        restaurant.products p,
-        restaurant.restaurant_products rp
-    WHERE r.id = rp.restaurant_id AND p.id = rp.product_id
+ SELECT r.id AS restaurant_id,
+    r.name AS restaurant_name,
+    r.active AS restaurant_active,
+    p.id AS product_id,
+    p.name AS product_name,
+    p.price AS product_price,
+    p.available AS product_available
+   FROM restaurant.restaurants r,
+    restaurant.products p,
+    restaurant.restaurant_products rp
+  WHERE r.id = rp.restaurant_id AND p.id = rp.product_id
 WITH DATA;
 
 refresh materialized VIEW restaurant.order_restaurant_m_view;
@@ -91,13 +87,13 @@ refresh materialized VIEW restaurant.order_restaurant_m_view;
 DROP function IF EXISTS restaurant.refresh_order_restaurant_m_view;
 
 CREATE OR replace function restaurant.refresh_order_restaurant_m_view()
-return trigger
+returns trigger
 AS '
 BEGIN
     refresh materialized VIEW restaurant.order_restaurant_m_view;
     return null;
 END;
-' LANGUAGE plpgsql;
+'  LANGUAGE plpgsql;
 
 DROP trigger IF EXISTS refresh_order_restaurant_m_view ON restaurant.restaurant_products;
 
