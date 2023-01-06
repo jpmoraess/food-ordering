2d2247208a0e6709380cc5e546f13ff0e035d876
jpmoraess <moraess.it@gmail.com>
2023-01-06 00:33:28 -0300

    Implementing Customer Service modules using Clean and Hexagonal Architecture principles

---
+++ customer-service/customer-domain/customer-application-service/src/main/java/com/food/ordering/system/customer/service/domain/mapper/CustomerDataMapper.java
@@ -0,0 +1,25 @@
+package com.food.ordering.system.customer.service.domain.mapper;
+
+import com.food.ordering.system.customer.service.domain.create.CreateCustomerCommand;
+import com.food.ordering.system.customer.service.domain.create.CreateCustomerResponse;
+import com.food.ordering.system.customer.service.domain.entity.Customer;
+import com.food.ordering.system.domain.valueobject.CustomerId;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerDataMapper {
+
+    public Customer createCustomerCommandToCustomer(CreateCustomerCommand createCustomerCommand) {
+        return new Customer(new CustomerId(createCustomerCommand.getCustomerId()),
+                createCustomerCommand.getUsername(),
+                createCustomerCommand.getFirstName(),
+                createCustomerCommand.getLastName());
+    }
+
+    public CreateCustomerResponse customerToCreateCustomerResponse(Customer customer, String message) {
+        return CreateCustomerResponse.builder()
+                .customerId(customer.getId().getValue())
+                .message(message)
+                .build();
+    }
+}
---
+++ customer-service/customer-messaging/src/main/java/com/food/ordering/system/customer/service/messaging/mapper/CustomerMessagingDataMapper.java
@@ -0,0 +1,18 @@
+package com.food.ordering.system.customer.service.messaging.mapper;
+
+import com.food.ordering.system.customer.service.domain.event.CustomerCreatedEvent;
+import com.food.ordering.system.kafka.order.avro.model.CustomerAvroModel;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerMessagingDataMapper {
+
+    public CustomerAvroModel customerCreatedEventToCustomerAvroModel(CustomerCreatedEvent customerCreatedEvent) {
+        return CustomerAvroModel.newBuilder()
+                .setId(customerCreatedEvent.getCustomer().getId().getValue())
+                .setUsername(customerCreatedEvent.getCustomer().getUsername())
+                .setFirstName(customerCreatedEvent.getCustomer().getFirstName())
+                .setLastName(customerCreatedEvent.getCustomer().getLastName())
+                .build();
+    }
+}
