2013e4f07e59bf33283d8c735825830a56869741
jpmoraess <moraess.it@gmail.com>
2023-01-05 23:29:47 -0300

    Creating Customer kafka topic & Customer modules

---
+++ customer-service/customer-container/src/main/resources/init-schema.sql
@@ -0,0 +1,13 @@
+DROP SCHEMA IF EXISTS customer CASCADE;
+CREATE SCHEMA customer;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE customer.customers
+(
+    id uuid NOT NULL,
+    username character varying COLLATE pg_catalog."default" NOT NULL,
+    first_name character varying COLLATE pg_catalog."default" NOT NULL,
+    last_name character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT customers_pkey PRIMARY KEY (id)
+);
